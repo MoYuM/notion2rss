e62e4c2a43725d60c27e57dcc2f41466e767c798
MoYuM <hanbaochang12138@gmail.com>
2025-05-01 13:51:31 +0800

    更多环境变量

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -1,46 +1,58 @@
-import { Client } from '@notionhq/client'
-import { NextResponse } from 'next/server'
-import RSS from 'rss'
+import { Client } from "@notionhq/client";
+import { NextResponse } from "next/server";
+import RSS from "rss";
 
 const client = new Client({
-    auth: process.env.NOTION_TOKEN,
-})
+  auth: process.env.NOTION_TOKEN,
+});
 
 export async function GET() {
-    const response = await client.databases.query({ database_id: "969d8b77bb734f3eaa1cc00a245f93d7" })
-    
-    // 创建 RSS feed
-    const feed = new RSS({
-        title: '面试题 RSS Feed',
-        description: '面试题更新通知',
-        feed_url: 'http://localhost:3000/api/rss',
-        site_url: 'http://localhost:3000',
-        language: 'zh-CN',
-        pubDate: new Date(),
-    })
+  if (!process.env.NOTION_DATABASE_ID) {
+    return new NextResponse("Notion database ID is not set", { status: 500 });
+  }
 
-    // 添加每个页面作为 RSS 条目
-    response.results.forEach((page: any) => {
-        const title = page.properties['页面'].title[0]?.plain_text || '无标题'
-        const url = page.public_url
-        const lastEditedTime = new Date(page.last_edited_time)
-        
-        feed.item({
-            title: title,
-            description: `分类: ${page.properties['面试题分类'].select?.name || '未分类'}`,
-            url: url,
-            date: lastEditedTime,
-            categories: [
-                page.properties['面试题分类'].select?.name,
-                page.properties['频率'].select?.name
-            ].filter(Boolean),
-        })
-    })
+  if (!process.env.NOTION_TOKEN) {
+    return new NextResponse("Notion token is not set", { status: 500 });
+  }
 
-    // 设置响应头为 XML
-    return new NextResponse(feed.xml(), {
-        headers: {
-            'Content-Type': 'application/xml',
-        },
-    })
+  if (!process.env.BASE_URL) {
+    return new NextResponse("Base URL is not set", { status: 500 });
+  }
+
+  const title = process.env.TITLE || "title";
+  const language = process.env.LANGUAGE || "zh-CN";
+
+  const response = await client.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+
+  // 创建 RSS feed
+  const feed = new RSS({
+    title,
+    language,
+    feed_url: `${process.env.BASE_URL}/rss`,
+    site_url: process.env.BASE_URL,
+    pubDate: new Date(),
+  });
+
+  response.results.forEach((page: any) => {
+    const title = page.properties["名称"].title[0]?.plain_text || "无标题";
+    const url = page.public_url;
+    const lastEditedTime = new Date(page.last_edited_time);
+
+    feed.item({
+      title: title,
+      description: "",
+      url: url,
+      date: lastEditedTime,
+      categories: page.properties["分类"]?.filter(Boolean),
+    });
+  });
+
+  // 设置响应头为 XML
+  return new NextResponse(feed.xml(), {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
 }
